c29dee17bc6fc8d34345b18a826b30cf06e91163
yuyijin <yuteru17@gmail.com>
2017-04-21 11:47:06 +0800

    2017mid

---
+++ Assets/follow.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+public class follow : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+    bool bnear = false;
+    void Update()
+    {
+        bool bk = Input.GetKey(KeyCode.Q);
+        if (bk)
+            bnear = true;
+    }
+    void LateUpdate()
+    {
+        if (bnear)
+        {
+            transform.position = player.transform.position + offset + new Vector3(0, -1, 0);
+        }
+        else
+        {
+            transform.position = player.transform.position + offset;
+        }
+    }
+}
